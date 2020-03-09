12aeaace2d06d0d38a7c927c719ec119e3372c25
EgoriousZhuzh <egorproger@gmail.com>
2020-03-09 15:51:27 +1000

    Миграции и модель Settings
    Settings - модель настроек сайта

---
+++ database/migrations/2020_03_09_052119_create_settings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string("site_title");
+            $table->string("site_subtitle");
+            $table->timestamps();
+        });
+
+        DB::table('settings')->insert(
+            array(
+                'site_title' => 'Web-site Title',
+                'site_subtitle' => 'some random text',
+                'created_at' => Carbon\Carbon::now(),
+                'updated_at' => Carbon\Carbon::now()
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
