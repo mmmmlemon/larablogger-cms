647311c2eac0922db8d18ad18ccee804490c218c
EgoriousZhuzh <egorproger@gmail.com>
2020-03-11 15:35:44 +1000

    Дизайн панели управления - заготовка

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,9 @@
   <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
   <link href="{{ asset('css/bulma_override.css') }}" rel="stylesheet">
 
+  <!-- Bulma Extensions -->
   <link href="{{ asset('css/bulma-tooltip.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/bulma-divider.min.css') }}" rel="stylesheet">
 
   <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
--- resources/views/user/control_panel.blade.php
+++ resources/views/user/control_panel.blade.php
@@ -1,19 +1,103 @@
 @extends('layouts.app')
 
 @section('content')
-
-    
-<section class="hero is-medium is-bold">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-         CONTROL PANEL
-        </h1>
-        <h2 class="subtitle">
-          Здесь будет панель управления сайтом
-        </h2>
-      </div>
+   
+<div class="container white-bg">
+  <div class="columns">
+    <div class="column is-4">
+        <button class="button is-link is-medium">
+          <span class="icon">
+            <i class="fas fa-pen"></i>
+          </span>
+          <span>Create post</span>
+        </button>
     </div>
-  </section>
+  
+  </div>
+  <div class="tabs is-boxed is-centered is-medium">
+    <ul>
+      <li class="is-active">
+        <a>
+          <span class="icon is-small"><i class="fas fa-cog" aria-hidden="true"></i></span>
+          <span>Settings</span>
+        </a>
+      </li>
+      <li>
+        <a>
+          <span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
+          <span>Users</span>
+        </a>
+      </li>
+      <li>
+        <a>
+          <span class="icon is-small"><i class="fas fa-file-alt" aria-hidden="true"></i></span>
+          <span>~</span>
+        </a>
+      </li>
+      <li>
+        <a>
+          <span class="icon is-small"><i class="far fa-file-alt" aria-hidden="true"></i></span>
+          <span>~</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="columns">
+    <div class="column is-12 has-text-centered">
+      <h3 class="subtitle">Web-site settings</h3>
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Site title</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded">
+              <input class="input" type="text" placeholder="Web-site name">
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Site subtitle</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded">
+              <input class="input" type="text" placeholder="Subtitle">
+            </p>
+          </div>
+        </div>
+      </div>
+    
+      <div class="field is-horizontal">
+        <div class="field-label">
+          <!-- Left empty for spacing -->
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <button type="submit" class="button is-link">
+                <span class="icon">
+                  <i class="fas fa-save"></i>
+                </span>
+                <span>
+                  Save
+                </span>
+            </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+</div>
 
 @endsection
