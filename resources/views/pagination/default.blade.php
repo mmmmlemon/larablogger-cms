12a9ff38d86b11ed56485b4d5d4877fefd083cf4
EgoriousZhuzh <egorproger@gmail.com>
2020-03-19 13:32:33 +1000

    Фикс пагинатора

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -1,12 +1,20 @@
 
-<nav class="pagination" role="navigation" aria-label="pagination">
+<nav class="pagination is-centered" role="navigation" aria-label="pagination">
     @if ($paginator->lastPage() > 1)
-    <a href="{{ $paginator->url(1) }}" class="pagination-previous"><</a>
-    <a href="{{ $paginator->url($paginator->currentPage()+1) }}" class="pagination-next">></a>
+<a href="{{ $paginator->url(1) }}" class="pagination-previous" @if($paginator->currentPage() == 1) disabled @endif>
+  <span class="icon">
+    <i class="fas fa-chevron-left" ></i>
+  </span>
+</a>
+    <a @if($paginator->currentPage() != $paginator->lastPage()) href="{{ $paginator->url($paginator->currentPage()+1) }}" @endif class="pagination-next"  @if($paginator->currentPage() == $paginator->lastPage()) disabled @endif>
+      <span class="icon">
+        <i class="fas fa-chevron-right" ></i>
+      </span>
+    </a>
     <ul class="pagination-list">
     @for ($i = 1; $i <= $paginator->lastPage(); $i++)
       <li>
-        <a href="{{ $paginator->url($i) }}" class="{{ ($paginator->currentPage() == $i) ? ' active' : '' }} pagination-link" aria-label="Goto page 1">{{ $i }}</a>
+        <a href="{{ $paginator->url($i) }}" class="{{ ($paginator->currentPage() == $i) ? ' is-current' : '' }} pagination-link" aria-label="Goto page 1">{{ $i }}</a>
       </li>
       @endfor
     </ul>
