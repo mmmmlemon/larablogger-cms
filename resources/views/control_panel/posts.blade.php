5b8e140301e06d3b1e16a52afe692c0737310569
EgoriousZhuzh <egorproger@gmail.com>
2020-03-25 16:24:28 +1000

    пуньк

--- resources/views/control_panel/posts.blade.php
+++ resources/views/control_panel/posts.blade.php
@@ -1,6 +1,11 @@
 <div id="posts_content" class="invisible">
-    @foreach($posts as $post)
-        <h1>{{$post->post_title}}</h1>
-    
-    @endforeach
+    @if($posts->count() > 0)
+        @foreach($posts as $post)
+            <h1>{{$post->post_title}}</h1>
+        
+        @endforeach
+    @else
+    <h1>No posts yet</h1>
+    <a href="/control/create_post" class="button is-link">Create post</a>
+    @endif
     </div>
