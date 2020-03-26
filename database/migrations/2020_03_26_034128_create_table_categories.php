813c5e596d8844de3e434d4b8627cd6ffd56afd4
EgoriousZhuzh <egorproger@gmail.com>
2020-03-26 13:49:48 +1000

    Создание таблицы категорий
    Создал таблицу categories через миграцию и создал для неё модель

---
+++ database/migrations/2020_03_26_034128_create_table_categories.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name');
+            $table->timestamps();
+        });
+
+        DB::table('categories')->insert(
+            array(
+                array(
+                    'category_name' => 'Videos',
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now()
+                ),
+                array(
+                    'category_name' => 'Gallery',
+                    'created_at' => Carbon\Carbon::now(),
+                    'updated_at' => Carbon\Carbon::now()
+                ),)
+        );
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
