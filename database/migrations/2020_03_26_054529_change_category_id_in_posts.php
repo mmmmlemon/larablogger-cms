5ace72f0c6008ac3602dd9b6325e919ad80daaca
EgoriousZhuzh <egorproger@gmail.com>
2020-03-26 16:08:50 +1000

    Добавление поля categpry_id (fix)
    Добавление поля category_id к таблице posts и добавление внешнего ключа

--- database/migrations/2020_03_26_054529_change_category_id_in_posts.php
+++ database/migrations/2020_03_26_054529_change_category_id_in_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCategoryIdToPosts extends Migration
+class ChangeCategoryIdInPosts extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddCategoryIdToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->unsignedBigInteger('category_id');
-            
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
@@ -27,7 +26,7 @@ class AddCategoryIdToPosts extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('category_id');
+            //
         });
     }
 }
