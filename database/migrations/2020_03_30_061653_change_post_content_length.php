dfbd78b44bf21f647ab218066f144cddc6787576
EgoriousZhuzh <egorproger@gmail.com>
2020-03-30 16:20:55 +1000

    Изменение длины post_content
    Изменение длины поля post_content у posts, с 255 на 1000

---
+++ database/migrations/2020_03_30_061653_change_post_content_length.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePostContentLength extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function ($table) {
+            $table->string('post_content', 1000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function ($table) {
+            $table->string('post_content', 255)->change();
+        });
+    }
+}
