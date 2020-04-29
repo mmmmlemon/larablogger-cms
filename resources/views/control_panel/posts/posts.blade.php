96bf408dc49d8d8e1397f387887e8374c1090622
mmmm_lemon <egorproger@gmail.com>
2020-04-29 13:42:29 +1000

    Вернул модальное окно в posts
    Случайно удалил в предыдущем коммите

--- resources/views/control_panel/posts/posts.blade.php
+++ resources/views/control_panel/posts/posts.blade.php
@@ -118,7 +118,30 @@
 @endsection
 
 @section('modals')
-
+<div class="modal modalDelete">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">You sure?</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <p>Are you sure you want to delete this post?</p>
+        <b id="modal_post_title"></b>
+        <p>This action will also delete all media attached to this post.</p>
+        <p class="has-text-danger">This action cannot be undone.</p>
+      </section>
+      <footer class="modal-card-foot">
+          <form id="modal_form" action="/control/delete_post" method="post" style="display:inline;">
+                @method('DELETE')
+                @csrf
+                <input type="text" class="invisible" id="modal_form_input" name="modal_form_input">
+        </form>
+        <button class="button is-danger" id="submit_modal">Delete</button>
+        <button class="button cancel">Cancel</button>
+      </footer>
+    </div>
+  </div>
 @endsection
 
 @push('scripts')
