6d6f542a4efb4b80c9707b5c755b22ce744fb10e
mmmm_lemon <egorproger@gmail.com>
2020-05-08 13:32:22 +1000

    Миграция - новые поля у медиа

---
+++ database/migrations/2020_05_08_030802_add_columns_to_media.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToMedia extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->string('display_name');
+            $table->string('actual_name');
+            $table->integer('visibility')->default(1);
+            $table->string('thumbnail_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->dropColumn('display_name');
+            $table->dropColumn('actual_name');
+            $table->dropColumn('visibility');
+            $table->dropColumn('thumbnail_url');
+        });
+    }
+}
