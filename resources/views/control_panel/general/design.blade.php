1a3388d1eff50893874fcfd059bbd292eee215fc
mmmm_lemon <egorproger@gmail.com>
2020-05-20 12:26:47 +1000

    Дизайн - новые поля
    Добавил новые поля на страницу дизайна

--- resources/views/control_panel/general/design.blade.php
+++ resources/views/control_panel/general/design.blade.php
@@ -17,7 +17,7 @@
                 <div class="field-body">
                     <div id="bg-img" class="file has-name">
                         <label class="file-label">
-                          <input class="file-input" type="file" name="background_image">
+                          <input class="file-input" type="file" id="background_image" name="background_image">
                           <span class="file-cta">
                             <span class="file-icon">
                               <i class="fas fa-upload"></i>
@@ -46,6 +46,34 @@
                     </div>
                     
                     </div>
+                    <div class="is-divider"></div>
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                           
+                        </div>
+                        <div class="field-body">
+                            <input class="is-checkradio is-link" name="show_about" id="show_about" type="checkbox" checked>
+                            <label class="label" for="show_about">Show 'About' page</label>
+                            <a href="/control/edit_about" class="button is-link">
+                                <span class="icon is-small">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                                <span>Edit 'About' page</span>
+                            </a>
+                        </div>
+                        
+                    </div>
+                    <div class="is-divider"></div>
+
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label">Footer contents</label>   
+                        </div>
+                        <div class="field-body">
+                            <textarea class="textarea" name="footer_content" id="footer_content"></textarea>
+                        </div>
+                    </div>
+
                     <div class="field is-horizontal">
                         <div class="field-label">
                         <!-- Left empty for spacing -->
@@ -72,15 +100,23 @@
 
 
 
-@push('script')
+@push('scripts')
+<script src="{{ asset('js/jquery.richtext.min.js') }}"></script>
 <script>
-    // const fileInput = document.querySelector('#file-js-example input[type=file]');
-    // fileInput.onchange = () => {
-    //     if (fileInput.files.length > 0) 
-    //         {
-    //             const fileName = document.querySelector('#file-js-example .file-name');
-    //             fileName.textContent = fileInput.files[0].name;
-    //         }
-    //     }
+    const fileInput = document.querySelector('#bg-img input[type=file]');
+    fileInput.onchange = () => {
+        if (fileInput.files.length > 0) 
+            {
+                const fileName = document.querySelector('#bg-img .file-name');
+                fileName.textContent = fileInput.files[0].name;
+            }
+        }
+
+    $('#footer_content').richText({
+    imageUpload:false,
+    videoEmbed:false,
+    fileUpload:false
+    });
+    
 </script>
 @endpush
