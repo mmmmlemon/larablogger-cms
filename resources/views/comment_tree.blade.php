146234258016be867b62dc2365c74042211bc975
mmmm_lemon <egorproger@gmail.com>
2020-07-17 15:22:20 +1000

    Мелкие фиксы

--- resources/views/comment_tree.blade.php
+++ resources/views/comment_tree.blade.php
@@ -1,7 +1,6 @@
 @foreach($comments as $comment)
     @yield('comment', View::make('comment', compact('comment','is_admin')))
     <div class="comment_container">
-        @include('recursion',['comments'=>$comment->replies])
+        @include('comment_tree',['comments'=>$comment->replies])
     </div>
-
 @endforeach
