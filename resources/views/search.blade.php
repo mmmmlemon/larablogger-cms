bf70296a73e5319c0ca4a28c97ccf21e3128d1b5
mmmm_lemon <egorproger@gmail.com>
2020-07-31 15:34:50 +1000

    Поиск - плашка на случай если ничего не найдено

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,12 +7,20 @@
 <div class="container">
     <div class="white-bg">
     <h1 class="subtitle">Search results: {{count($results)}}</h1>
-        @foreach($results as $result)
-        <div class='white-bg search_full_results_block'>
-            <h1 class="subtitle"><a href="/post/{{$result->id}}">{{$result->post_title}}</a></h1>
-            <div class="post_content">{!!$result->post_content!!}</div>
-            <p><a href="/category/${el.category}">{{$result->category}}</a> | {{$result->date}}</p></div>
-        @endforeach
+        @if(count($results) == 0)
+        <div class="has-text-centered">
+            <h1 class="title">Nothing found</h1>
+            <i class="fas fa-search"></i>
+            <h1 class="subtitle">Try to search something different</h1>
+        </div>
+        @else
+            @foreach($results as $result)
+            <div class='white-bg search_full_results_block'>
+                <h1 class="subtitle"><a href="/post/{{$result->id}}">{{$result->post_title}}</a></h1>
+                <div class="post_content">{!!$result->post_content!!}</div>
+                <p><a href="/category/${el.category}">{{$result->category}}</a> | {{$result->date}}</p></div>
+            @endforeach
+        @endif
     </div>    
 </div>
 
