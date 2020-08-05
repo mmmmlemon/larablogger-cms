9cd8bde3c38fd257213c3092ca073b43c4423e15
mmmm_lemon <egorproger@gmail.com>
2020-08-05 14:39:39 +1000

    Дизайн

--- resources/views/control_panel/posts/posts.blade.php
+++ resources/views/control_panel/posts/posts.blade.php
@@ -8,26 +8,41 @@
           <li class="is-active"><a href="#" aria-current="page">Posts</a></li>
         </ul>
     </nav>
-    <div class="column">
-        <a href="/control" class="button is-link">
-            <span class="icon">
-                <i class="fas fa-arrow-left"></i>
-            </span>
-            <span>Back</span>
-        </a>
-        <a href="/control/create_post" class="button is-link">
-            <span class="icon">
-                <i class="fas fa-pen"></i>
-            </span>
-            <span>Add post</span>
-        </a>
-        <a href="/control/media" class="button is-link" data-tooltip="Add/edit categories">
-            <span class="icon">
-                <i class="fas fa-video"></i>
-            </span>
-            <span>Media browser</span>
-        </a>
+    @if(config('isMobile') != true)
+        <div class="column">
+            <a href="/control/create_post" class="button is-link">
+                <span class="icon">
+                    <i class="fas fa-pen"></i>
+                </span>
+                <span>Add post</span>
+            </a>
+            <a href="/control/media" class="button is-link" data-tooltip="Add/edit categories">
+                <span class="icon">
+                    <i class="fas fa-video"></i>
+                </span>
+                <span>Media browser</span>
+            </a>
+        </div>
+    @else
+    <div class="columns has-text-centered">
+        <div class="column is-12">
+            <div class="buttons is-centered ">
+                <a href="/control/create_post" class="button is-link">
+                    <span class="icon">
+                        <i class="fas fa-pen"></i>
+                    </span>
+                    <span>Add post</span>
+                </a>
+                <a href="/control/media" class="button is-link" data-tooltip="Add/edit categories">
+                    <span class="icon">
+                        <i class="fas fa-video"></i>
+                    </span>
+                    <span>Media browser</span>
+                </a>
+            </div>
+        </div>
     </div>
+    @endif
 
     <div class="is-divider"></div>
 
@@ -35,90 +50,156 @@
         <div class="column">
             <div class="columns">
                 @if($posts->count() > 0)
-                    <table class="table is-fullwidth is-hoverable">
-                        <thead>
-                            <th>Title</th>
-                            <th>Visibility</th>
-                            <th>Category</th>
-                            <th>
+                    @if(config('isMobile') != true)
+                        <table class="table is-fullwidth is-hoverable">
+                            <thead>
+                                <th>Title</th>
+                                <th>Visibility</th>
+                                <th>Category</th>
+                                <th>
+                                
+                                    <a @if($page=="normal")href="/control/posts/date"@else href="/control/posts"@endif>Date</a>  
+                                    @if($page=="normal")
+                                        <i class="fas fa-sort-down"></i></th>
+                                    @else 
+                                        <i class="fas fa-sort-up"></i></th>
+                                    @endif
+                                <th>Actions</th>
+                            </thead>
+                            <tbody>
+                                @foreach($posts as $post)
+                                    <tr>
                             
-                                <a @if($page=="normal")href="/control/posts/date"@else href="/control/posts"@endif>Date</a>  
-                                @if($page=="normal")
-                                    <i class="fas fa-sort-down"></i></th>
-                                @else 
-                                    <i class="fas fa-sort-up"></i></th>
-                                @endif
-                            <th>Actions</th>
-                        </thead>
-                        <tbody>
+                                        <td><b><a href="/post/{{$post->id}}">{{$post->post_title}}</a></b></td>
+                                        <td>
+                                
+                                            @if($post->visibility == 1)
+                                                <span class="icon is-small" data-tooltip="Post is visible">
+                                                    <i class="fas fa-check"></i>
+                                                </span>
+                                            @else
+                                                <span class="icon is-small" data-tooltip="Post is hidden">
+                                                    <i class="fas fa-times"></i>
+                                                </span>
+                                            @endif
+                                        </td>
+                        
+                                    <td><a href="/category/{{App\Category::find($post->category_id)->category_name}}">{{App\Category::find($post->category_id)->category_name}}</a></td>
+                                
+                                        <td>{{date('d.m.Y', strtotime($post->date))}}</td>
+                                        <td>
+                                
+                                            @if($post->pinned == 0)
+                                            
+                                                <form action="/control/pin_post" method="post" style="display:inline;">
+                                                    @csrf
+                                                    <input type="text" name="id" value="{{$post->id}}" class="invisible">
+                                                    <button class="button is-success" data-tooltip="Pin this post"><i class="fas fa-thumbtack"></i></button>
+                                                </form>
+                                            @else
+                                    
+                                                <form action="/control/pin_post" method="post" style="display:inline;">
+                                                    @csrf
+                                                    <input type="text" name="id" value="{{$post->id}}" class="invisible">
+                                                    <button class="button is-warning" data-tooltip="Unpin this post"><i class="fas fa-thumbtack"></i></button>
+                                                </form>
+                                            @endif
+                                            
+                                    
+                                            @if($post->visibility == 1)
+                                        
+                                                <form action="/control/post_status/{{$post->id}}/0" method="post" style="display:inline;">
+                                                    @csrf
+                                                    <button class="button is-warning" data-tooltip="Hide this post"><i class="fas fa-eye-slash"></i></button>
+                                                </form>
+                                            @else
+                                
+                                                <form action="/control/post_status/{{$post->id}}/1" method="post" style="display:inline;">
+                                                    @csrf
+                                                    <button class="button is-primary" data-tooltip="Show this post"><i class="fas fa-eye"></i></button>
+                                                </form>
+                                            @endif
+                            
+                                                <a href="/post/{{$post->id}}/edit" class="button is-info">
+                                                    <span class="icon is-small" data-tooltip="Edit this post">
+                                                        <i class="fas fa-edit"></i>
+                                                    </span>
+                                                </a>
+                    
+                                            <button class="button is-danger showModalDelete" 
+                                                data-tooltip="Delete this post" data-title="{{$post->post_title}}" data-id="{{$post->id}}">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>    
+                                @endforeach
+                            </tbody>  
+                        </table>
+                    @else
+                        <table class="table is-fullwidth is-hoverable ">
                             @foreach($posts as $post)
-                                <tr>
-                           
-                                    <td><b><a href="/post/{{$post->id}}">{{$post->post_title}}</a></b></td>
+                                <tr class="">
                                     <td>
-                               
+                                    <p class="has-text-centered" style="font-size: 14pt;"><a href="/post/{{$post->id}}"><b>{{$post->post_title}}</b></a></p>
+                                    <p class="has-text-centered" style="font-size: 10pt;">
+                                        {{date('d.m.y',strtotime($post->created_at))}} |
+                                        <a href="/category/{{App\Category::find($post->category_id)->category_name}}">{{App\Category::find($post->category_id)->category_name}}</a>
+                                        |
                                         @if($post->visibility == 1)
-                                            <span class="icon is-small" data-tooltip="Post is visible">
-                                                <i class="fas fa-check"></i>
-                                            </span>
+                                            <i class="fas fa-eye"></i>
                                         @else
-                                            <span class="icon is-small" data-tooltip="Post is hidden">
-                                                <i class="fas fa-times"></i>
-                                            </span>
+                                            <i class="fas fa-eye-slash"></i>
                                         @endif
-                                    </td>
-                       
-                                    <td>{{App\Category::find($post->category_id)->category_name}}</td>
-                            
-                                    <td>{{date('d.m.Y', strtotime($post->date))}}</td>
-                                    <td>
-                               
-                                        @if($post->pinned == 0)
-                                          
+                                    </p>
+                                    
+                                    <div class="buttons has-addons is-centered" style="margin-top:5px;">
+                                        @if($post->pinned == 0)  
                                             <form action="/control/pin_post" method="post" style="display:inline;">
-                                                @csrf
-                                                <input type="text" name="id" value="{{$post->id}}" class="invisible">
                                                 <button class="button is-success" data-tooltip="Pin this post"><i class="fas fa-thumbtack"></i></button>
-                                            </form>
-                                        @else
-                                   
-                                            <form action="/control/pin_post" method="post" style="display:inline;">
                                                 @csrf
                                                 <input type="text" name="id" value="{{$post->id}}" class="invisible">
+                                            </form>
+                                         @else
+                                            <form action="/control/pin_post" method="post" style="display:inline;">
                                                 <button class="button is-warning" data-tooltip="Unpin this post"><i class="fas fa-thumbtack"></i></button>
+                                                @csrf
+                                                <input type="text" name="id" value="{{$post->id}}" class="invisible">    
                                             </form>
                                         @endif
                                         
-                                  
+                                        
                                         @if($post->visibility == 1)
-                                      
                                             <form action="/control/post_status/{{$post->id}}/0" method="post" style="display:inline;">
                                                 @csrf
                                                 <button class="button is-warning" data-tooltip="Hide this post"><i class="fas fa-eye-slash"></i></button>
                                             </form>
                                         @else
-                               
                                             <form action="/control/post_status/{{$post->id}}/1" method="post" style="display:inline;">
                                                 @csrf
-                                                <button class="button is-primary" data-tooltip="Show this post"><i class="fas fa-eye"></i></button>
+                                                <button class="button is-primary" style="border-radius
+                                                
+                                                :0px;" data-tooltip="Show this post"><i class="fas fa-eye"></i></button>
                                             </form>
                                         @endif
-                         
-                                            <a href="/post/{{$post->id}}/edit" class="button is-info">
-                                                <span class="icon is-small" data-tooltip="Edit this post">
-                                                    <i class="fas fa-edit"></i>
-                                                </span>
-                                            </a>
-                 
+
+                                        <a href="/post/{{$post->id}}/edit" class="button is-info">
+                                            <span class="icon is-small" data-tooltip="Edit this post">
+                                                <i class="fas fa-edit"></i>
+                                            </span>
+                                        </a>
+
                                         <button class="button is-danger showModalDelete" 
                                             data-tooltip="Delete this post" data-title="{{$post->post_title}}" data-id="{{$post->id}}">
                                             <i class="fas fa-trash"></i>
-                                        </button>
+                                        </button>                                  
+                                    </div>
+                        
                                     </td>
-                                </tr>    
+                          
+                                </tr>
                             @endforeach
-                        </tbody>  
-                    </table>
+                        </table>
+                    @endif
                 @else
 
                 <div class="column has-text-centered">
