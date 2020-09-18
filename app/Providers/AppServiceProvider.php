c8fe9777909b349995b473dbe9b935e0de56107b
mmmm_lemon <egorproger@gmail.com>
2020-09-18 16:51:14 +1000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,28 +32,28 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         
-        Schema::defaultStringLength(191);
+    //     Schema::defaultStringLength(191);
 
-        //check if mobile or desktop
-        $agent = new Agent();
-        $isMobile = $agent->isMobile();
-        config(['isMobile' => $isMobile]);
+    //     //check if mobile or desktop
+    //     $agent = new Agent();
+    //     $isMobile = $agent->isMobile();
+    //     config(['isMobile' => $isMobile]);
 
-        //site title
-        $site_title = App\Settings::all()[0]->site_title;
-        config(['site_title' => $site_title]);
+    //     //site title
+    //     $site_title = App\Settings::all()[0]->site_title;
+    //     config(['site_title' => $site_title]);
 
-        //app.blade
-        //settings
-        $settings = App\Settings::all()->first();
-        config(['settings' => $settings]);
+    //     //app.blade
+    //     //settings
+    //     $settings = App\Settings::all()->first();
+    //     config(['settings' => $settings]);
 
-        //social media
-        $social_media = App\SocialMedia::whereNotNull('platform_name')->whereNotNull('url')->get();
-        config(['social_media' => $social_media]);
+    //     //social media
+    //     $social_media = App\SocialMedia::whereNotNull('platform_name')->whereNotNull('url')->get();
+    //     config(['social_media' => $social_media]);
 
-        //categoires
-        $categories = App\Category::where('category_name','!=','blank')->orderBy('visual_order','asc')->get();
-        config(['categories' => $categories]);
-    }
+    //     //categoires
+    //     $categories = App\Category::where('category_name','!=','blank')->orderBy('visual_order','asc')->get();
+    //     config(['categories' => $categories]);
+    // }
 }
